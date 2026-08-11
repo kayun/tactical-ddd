c22cc5959e6e997e39f08b182c7b7e4df8985d26
Artyom Kayun <kayun.artem@gmail.com>
2026-08-11 23:33:39 +0200

    feat(domain): update `<%= interfaceName %>` to use `@tactical-ddd` facade types
    - Replaced the custom interface with `Facade` from `@tactical-ddd/core` for standardized query, watch, and command handling.

--- packages/nx/src/generators/domain/files/contracts/src/lib/interfaces/__name__-facade.interface.ts
+++ packages/nx/src/generators/domain/files/contracts/src/lib/interfaces/__name__-facade.interface.ts
@@ -1,6 +1,16 @@
-export interface <%= interfaceName %> {
-  method(): void
-}
+import type { Command, Facade, Query, Watch } from '@tactical-ddd/core';
+
+export type <%= interfaceName %> = Facade<{
+  queries: {
+    query(): Query<unknown>;
+  };
+  watches: {
+    watch(): Watch<unknown>;
+  };
+  commands: {
+    command(): Command;
+  };
+}>;
 
 export const <%= interfaceName %> = {
   $: Symbol.for('<%= interfaceName %>')
