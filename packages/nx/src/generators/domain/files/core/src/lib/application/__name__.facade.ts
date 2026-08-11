798248b44bf052d8cea75f18c899b385ea40b072
Artyom Kayun <kayun.artem@gmail.com>
2026-08-11 23:33:51 +0200

    feat(domain): add `query` and `watch` methods to `<%= interfaceName %>` facade
    - Implemented `query` and `watch` methods using `@tactical-ddd/core` facade types for alignment with standardized interface structures.

--- packages/nx/src/generators/domain/files/core/src/lib/application/__name__.facade.ts
+++ packages/nx/src/generators/domain/files/core/src/lib/application/__name__.facade.ts
@@ -1,7 +1,17 @@
+import type { Command, Query, Watch } from '@tactical-ddd/core';
+
 import { <%= interfaceName %> } from '<%= contractsPackage %>';
 
 export class Core<%= interfaceName %> implements <%= interfaceName %> {
-  method(): void {
+  query(): Query<unknown> {
+    throw new Error('Method not implemented.');
+  }
+
+  watch(): Watch<unknown> {
+    throw new Error('Method not implemented.');
+  }
+
+  command(): Command {
     throw new Error('Method not implemented.');
   }
 }
